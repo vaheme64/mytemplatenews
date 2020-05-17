2ec7c0ee64e8789b684559edc8da6b84cc05bcc2
vaheme64 <vaheme64@yahoo.com>
2020-05-17 21:00:33 +0430

    create model permission and role and edit user model

---
+++ app/Permission.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    protected $fillable=['name','description'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+}
--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
