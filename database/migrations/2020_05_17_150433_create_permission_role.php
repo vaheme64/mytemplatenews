4effe7089d6e05b7a1d21296b368bee97d01fd4a
vaheme64 <vaheme64@yahoo.com>
2020-05-17 19:57:42 +0430

    make migration code complete

--- database/migrations/2020_05_17_150433_create_permission_role.php
+++ database/migrations/2020_05_17_150433_create_permission_role.php
@@ -13,10 +13,41 @@ class CreatePermissionRole extends Migration
      */
     public function up()
     {
-        Schema::create('permission_role', function (Blueprint $table) {
+        Schema::create('permissions', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
+        Schema::create('permission_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('permission_id');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->primary(['permission_id','user_id']);
+        });
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->primary(['role_id','user_id']);
+        });
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->unsignedBigInteger('permission_id');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->primary(['permission_id','role_id']);
+        });
+
     }
 
     /**
@@ -26,6 +57,10 @@ class CreatePermissionRole extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('permission_user');
+        Schema::dropIfExists('role_user');
         Schema::dropIfExists('permission_role');
+        Schema::dropIfExists('permissions');
+        Schema::dropIfExists('roles');
     }
 }
