542a7c275b231b5106a7bbaa4e93b32633a3c504
vaheme64 <vaheme64@yahoo.com>
2020-05-17 16:46:10 -0700

    modified login and register , make admin route

---
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminAuthenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($request->user()->isSuperUser() || $request->user()->isStaff()){
+            return $next($request);
+        }
+        return redirect('/');
+
+    }
+}
